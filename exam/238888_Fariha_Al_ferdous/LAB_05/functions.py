7f175e8b8fe575f762291807af99b23131df3b47
farihachaiti <farihaalferdous@gmail.com>
2023-12-30 07:47:25 +0100

    update-lab5

--- exam/238888_Fariha_Al_ferdous/LAB_05/functions.py
+++ exam/238888_Fariha_Al_ferdous/LAB_05/functions.py
@@ -1,2 +1,51 @@
 # Add the class of your model only
 # Here is where you define the architecture of your model using pytorch
+
+def PCFG_rule():
+        #PCFG
+    wght_rules = [
+        'S -> NP VP PP CONJP [1.0]',
+        'NP -> N VP [0.5]',
+        'NP -> PRON VP [0.5]',
+        'VP -> V PP [0.7]',
+        'VP -> V N [0.3]',
+        'PP -> P DET N [0.3]',
+        'PP -> P N [0.2]',
+        'PP -> P VP [0.5]',
+        'CONJP -> CC NP VP [0.6]',
+        'CONJP -> CC N [0.4]',
+        'PRON -> "I" [0.5]',
+        'PRON -> "She" [0.5]', 
+        'P -> "with" [1.0]',
+        'N -> "Margaret" [0.5]',
+        'N -> "rice" [0.1]',
+        'N -> "fish" [0.1]', 
+        'N -> "beef" [0.1]',
+        'N -> "dance" [0.1]',
+        'N -> "photography" [0.1]',
+        'V -> "like" [0.25]',
+        'V -> "do" [0.25]',
+        'V -> "ate" [0.25]',
+        'V -> "loves" [0.25]',
+        'DET -> "a" [1.0]',
+        'CC -> "and" [0.5]',
+        'CC -> "but" [0.5]',
+        'ADV -> "also" [0.5]',
+        'ADV -> "not" [0.5]',
+    ]
+
+    return wght_rules
+
+
+def parse_sentences(sentences, parser):
+    for sent in sentences:
+        for tree in parser.parse(sent.split()):
+            print(tree)
+
+def print_sentences(func, grammer=None, start=None, n=10):
+    if grammer:
+        for sent in func(grammer, start=start, n=n):
+            print(sent)
+    else:
+        for sent in func(n):
+            print(sent)
--- exam/238888_Fariha_Al_ferdous/LAB_05/main.py
+++ exam/238888_Fariha_Al_ferdous/LAB_05/main.py
@@ -3,7 +3,48 @@
 
 # Import everything from functions.py file
 from functions import *
+import nltk
+from nltk.parse.generate import generate
+from nltk import Nonterminal
+from pcfg import PCFG
+
+
 
 if __name__ == "__main__":
     #Wrtite the code to load the datasets and to run your functions
     # Print the results
+        #2 sentences
+    sentences = ["Margaret ate rice with a fish but I do not like fish", "She loves photography and also dance"]
+
+
+    print("\033[1m2 Sentences:\033[0m")
+    for sent in sentences:   
+        print(sent)
+
+        
+    print("\033[1mPCFG Model:\033[0m")
+    wght_rules = PCFG_rule()
+    for rule in wght_rules:
+        print(rule)
+
+    grammer = nltk.PCFG.fromstring(wght_rules)
+    parser = nltk.ViterbiParser(grammer)
+
+
+    print("\033[1mParsing of Sentences:\033[0m")
+    parse_sentences(sentences, parser)
+
+
+
+    print("\033[1m10 sentences with nltk.parse.generate.generate using starting symbole 'NP'\033[0m") 
+    start = Nonterminal('NP')
+    print_sentences(generate, grammer=grammer, start=start, n=10)
+
+    print("\033[1m10 sentences with nltk.parse.generate.generate using starting symbol 'VP'\033[0m") 
+    start = Nonterminal('VP')
+    print_sentences(generate, grammer=grammer, start=start, n=10)
+
+    #10 sentences with PCFG.generate    
+    print("\033[1m10 sentences with PCFG.generate\033[0m") 
+    toy_grammar = PCFG.fromstring(wght_rules)
+    print_sentences(toy_grammar.generate, grammer=None, start=None, n=10)
