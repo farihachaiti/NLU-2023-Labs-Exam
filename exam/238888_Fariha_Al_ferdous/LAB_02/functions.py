e6b4436248bf091a2ddbc5956990830525b6ef74
farihachaiti <farihaalferdous@gmail.com>
2024-01-15 22:06:52 +0100

    update

--- exam/238888_Fariha_Al_ferdous/LAB_02/functions.py
+++ exam/238888_Fariha_Al_ferdous/LAB_02/functions.py
@@ -4,24 +4,29 @@
 from sklearn.model_selection import cross_validate
 from sklearn.metrics import classification_report
 from sklearn.datasets import fetch_20newsgroups
+import numpy as np
 
 #function to evaluate the classifier
 def SVM_evaluation(clf, data, split):
+    hyps_list = []
+    refs_list = []
+    scores_list= []
     # for each training/testing fold
     for train_index, test_index in split.split(data.data, data.target):
         # train (fit) model
         clf.fit(data.data[train_index], data.target[train_index])
         # predict test labels
         clf.predict(data.data[test_index])
-        # score the model (using average accuracy for now)
-        accuracy = clf.score(data.data[test_index], data.target[test_index])
-        print("Accuracy: {:.3}".format(accuracy))
-    # predict test labels
-    hyps = clf.predict(data.data[test_index])
-    refs = data.target[test_index]
-    scores = cross_validate(clf, data.data, data.target, cv=split, scoring=['f1_weighted'])   
-        
-    print(classification_report(refs, hyps, target_names=data.target_names))
+        # predict test labels
+        hyps = clf.predict(data.data[test_index])
+        refs = data.target[test_index]
+        hyps_list.append(hyps)  
+        refs_list.append(refs)
+    refs_list2 = [item for sublist in refs_list for item in sublist]
+    hyps_list2 = [item for sublist in hyps_list for item in sublist]
+    scores = cross_validate(clf, data.data, data.target, cv=split, scoring=['f1_weighted']) 
+
+    print(classification_report(refs_list2, hyps_list2, target_names=data.target_names))
     print(sum(scores['test_f1_weighted'])/len(scores['test_f1_weighted']))
 
 #function to extract features
